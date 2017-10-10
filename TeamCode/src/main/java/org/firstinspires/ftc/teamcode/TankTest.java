50305c763bd37e0a77c3c3ac8c14ea89ae4438d6
agentx1118 <agentx1118@users.noreply.github.com>
2017-10-10 14:50:05 -0400

    Created a demo of a tank drive program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTest.java
@@ -0,0 +1,28 @@
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Sample Tank Drive", group = "Pushbot")
+public class TankTest extends LinearOpMode()
+{
+  VoltronHardwareConfig robot = new VoltronHardwareConfig();
+  
+  @Override
+  public void runOpMode()
+  {
+    robot.init(hardwareMap);
+    while(opModeIsActive())
+    {
+      double lPower = gamepad1.left_stick_y;
+      double rPower = gamepad1.right_stick_y;
+      robot.driveMotorL.setPower(lPower);
+      robot.driveMotorR.setPower(rPower);
+      
+      //adds telemetry data to phone display
+      telemetry.addData("Left_Drive", "%2f", lPower);
+      telemetry.addData("Right_Drive", "%2f", rPower);
+      telemetry.update();
+      robot.waitForTick(40);
+    }
+  }
+}
